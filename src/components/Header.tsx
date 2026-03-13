79984ed1fea4ecf0ebd027c209d608ac7f99d290
rimsakine <sakinerim042@gmail.com>
2026-03-13 01:44:46 +0000

    TP_Seance2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,17 @@
 import styles from "./Header.module.css";
 interface HeaderProps {
   title: string;
+  userName?: string;
+  onLogout?: () => void;
   onMenuClick: () => void;
 }
 export default function Header({ title, onMenuClick }: HeaderProps) {
   return (
     <header className={styles.header}>
       <div className={styles.left}>
-        <button className={styles.menuBtn} onClick={onMenuClick}>
-          ☰
-        </button>
         <h1 className={styles.logo}>{title}</h1>
       </div>
-      <span className={styles.avatar}>JD</span>
+      <span className={styles.avatar}>RS</span>
     </header>
   );
 }
