f8c414b40689e8f4a6bf10685a14ec2069a3db8f
rimsakine <sakinerim042@gmail.com>
2026-04-03 16:00:48 +0100

    tp4

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from "./features/auth/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
